6946f1de824cb9fdfbf538b7eaac4ff9d56ab490
ShannonCanTech <shecantech@gmail.com>
2020-02-02 03:55:16 -0800

    Implemented full lifecycle of CRUD web app by adding new methods for showing job details, updating details in jobform.html, and deleting individual jobs, all methods in HomeController.java

--- src/main/java/dev/shannonfoster/demo/HomeController.java
+++ src/main/java/dev/shannonfoster/demo/HomeController.java
@@ -3,8 +3,13 @@ package dev.shannonfoster.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -12,7 +17,7 @@ public class HomeController {
     @Autowired
     JobRepository jobRepository;
 
-    @RequestMapping
+    @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("jobs", jobRepository.findAll());
         return "index";
@@ -22,5 +27,32 @@ public class HomeController {
     public String addJob(Model model) {
         model.addAttribute("job", new Job());
         return "jobform";
+    }
+
+    @PostMapping("/process")
+    public String processForm(@Valid Job job, BindingResult result) {
+        if (result.hasErrors()) {
+            return "jobform";
+        }
+        jobRepository.save(job);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/details/{id}")
+    public String jobDetails(@PathVariable("id") long id, Model model) {
+        model.addAttribute("job", jobRepository.findById(id));
+        return "details";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateJob(@PathVariable("id") long id, Model model) {
+        model.addAttribute("job", jobRepository.findById(id));
+        return "jobform";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String deleteJob(@PathVariable("id") long id) {
+        jobRepository.deleteById(id);
+        return "redirect:/";
     }
 }
